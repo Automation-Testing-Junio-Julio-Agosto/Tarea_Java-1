d25a1b70674664766ee28b583516d9a36c4fd53b
Juan De Dios <jbaudazio@gmail.com>
2022-06-13 18:55:07 -0500

    tarea 1 ,ejercicio 1

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,2 +1,13 @@
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        var numero = 5;
+        var esPar=false;
+
+        if (numero % 2 == 0) {
+            esPar=true;
+            System.out.println("El resultado es: "+ esPar);
+        } else
+            System.out.println("El resultado es: "+esPar);
+    }
 }
+
--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,3 @@
 public class Ejercicio5 {
-    public static void main(String[] args) {
-        var numero=5;
-    }
+
 }
