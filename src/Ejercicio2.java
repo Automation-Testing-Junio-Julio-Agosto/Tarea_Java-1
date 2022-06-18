7e0a2514ab9bf44be7b0676d72fb821d552b995c
Juan De Dios <jbaudazio@gmail.com>
2022-06-18 17:36:55 -0500

    ejercicio 2 , terminado

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in); //inicializamos el scanner
+
+        System.out.print("Ingresar numero 1: ");
+        var numero1 = scanner.nextInt();
+
+        System.out.print("Ingresar numero 2: ");
+        var numero2 = scanner.nextInt();
+
+        System.out.print("Ingresar numero 3: ");
+        var numero3 = scanner.nextInt();
+
+        System.out.print("Ingresar numero 4: ");
+        var numero4 = scanner.nextInt();
+
+        System.out.print("Ingresar numero 5: ");
+        var numero5 = scanner.nextInt();
+
+        System.out.print("Ingresar numero 6: ");
+        var numero6 = scanner.nextInt();
+
+        scanner.close(); //cerrando el scanner
+
+        var concatenacion = String.valueOf(numero1) +
+                String.valueOf(numero2) +
+                String.valueOf(numero3) +
+                String.valueOf(numero4) +
+                String.valueOf(numero5) +
+                String.valueOf(numero6);
+        System.out.println("El resultado es: " + concatenacion);
+    }
 }
--- src/Tarea1.java
+++ src/Tarea1.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Tarea1 {
+public class Tarea1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
 }
