2a47fe9f4e5ba17b50c725eafd02d30cea14dd5b
Juan De Dios <jbaudazio@gmail.com>
2022-06-18 18:10:12 -0500

    ejercicio 4 , terminado

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,2 +1,10 @@
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        var mensaje1 ="hola";
+        var mensaje2="mundo";
+        var espacio =" ";
+        var concatenar= mensaje1.concat(espacio).concat(mensaje2);
+        System.out.println(concatenar);
+
+    }
 }
