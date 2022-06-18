3ced58a7bc0d87b18fcf125cdd747406c21e829e
Juan De Dios <jbaudazio@gmail.com>
2022-06-18 17:59:41 -0500

    ejercicio 3 , terminado

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,2 +1,18 @@
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        var decimal1=12.1;
+        var decimal2=2.4;
+        var decimal3=2.23;
+        var numero1=1;
+        var numero2=2;
+        var numero3=3;
+
+        System.out.println("El resultado es; " +
+                decimal1+
+                decimal2+
+                decimal3+
+                numero1+
+                numero2+
+                numero3);
+    }
 }
