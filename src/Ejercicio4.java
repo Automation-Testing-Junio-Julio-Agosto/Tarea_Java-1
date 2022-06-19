58162efa9bbe34ae0f92a68e1d18df0b35e8e11b
Juan De Dios <jbaudazio@gmail.com>
2022-06-18 20:47:39 -0500

    ejercicio 4 ,corregido

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -2,9 +2,8 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         var mensaje1 ="hola";
         var mensaje2="mundo";
-        var espacio ="";
+        var espacio =" ";
         var concatenar= mensaje1.concat(espacio).concat(mensaje2);
         System.out.println(concatenar);
-
     }
 }
